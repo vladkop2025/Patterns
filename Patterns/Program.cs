faf125a105ba848a2bed6902949108e69cc08dd1
vladkop2025 <vladkop@mail.ru>
2025-05-15 20:15:08 +0300

    SingletonRealExample

--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
-namespace TEST
+namespace SingletonRealExample
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // создаем объект приложения
+            Application app = new Application();
+
+            // запускаем наше приложение (внутри создается соединение с базой данных по переданному адресу)
+            app.Launch("10.30.60.80");
+            Console.WriteLine(app.DbConnection.Configuration);
+
+            // Теперь пробуем создать новое соединение с базой данных уже по другому адресу
+            app.DbConnection = DbConnection.GetConnectionInstance("10.30.60.81");
+            // у нас не получилось, так как объект уже существует
+            Console.WriteLine(app.DbConnection.Configuration);
         }
     }
 }
---
+++ Patterns/SingletonRealExample.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonRealExample
+{
+    class DbConnection
+    {
+        private static DbConnection Connection;
+
+        public string Configuration { get; private set; }
+
+        protected DbConnection(string configuration)
+        {
+            Configuration = configuration;
+        }
+        /// <summary>
+        /// Реализация Синглтона не позволяет нам плодить новые объекты, если объект уже существует
+        /// </summary>
+        public static DbConnection GetConnectionInstance(string dbServer)
+        {
+            if (Connection == null)
+                Connection = new DbConnection(dbServer);
+            return Connection;
+        }
+    }
+
+    class Application
+    {
+        public DbConnection DbConnection { get; set; }
+        public void Launch(string dbServer)
+        {
+            DbConnection = DbConnection.GetConnectionInstance(dbServer);
+        }
+    }
+}
